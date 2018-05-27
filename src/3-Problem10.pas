ab4746701bcbe3f2446376a560b5d351e192dc2d
KadekYuda <yudaikadek22@gmail.com>
2018-05-27 22:30:42 +0700

    revision on 3

--- src/3-Problem10.pas
+++ src/3-Problem10.pas
@@ -6,6 +6,7 @@ n : longint;
 { definisi fungsi Factorial }
 function Factorial(n : longint) : longint;
 { Mengembalikan nilai n! dalam bentuk long integer untuk memenuhi angka-angka besar }
+{ Prekondisi: n >= 0 }
 	
 	{ algoritma fungsi }
 	begin
@@ -23,5 +24,12 @@ function Factorial(n : longint) : longint;
 begin
 	write('Masukan N: ');
 	readln(n);
-	writeln('N! = ',Factorial(n));
+	if ( n >= 0 ) then
+	begin
+		writeln('N! = ',Factorial(n));
+	end
+	else
+	begin
+		writeln('Invalid. Tolong masukkan N lebih besar atau sama dengan 0.');
+	end;
 end.
